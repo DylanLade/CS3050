fd6ea3c34a0c01bcc48b64a4cec8ff499761547c
DylanLade <dylanlade98@gmail.com>
2018-10-03 21:19:28 -0500

    Started the new program layout

--- 3050HW2.c
+++ 3050HW2.c
@@ -12,7 +12,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void build_array(int** graph,int magnitude[], int check[], int max);
+typedef struct{
+    int visited;
+    int magnitude;
+    int adj[];
+} Vert;
+
+typedef struct{
+    int top;
+    int elements[];
+} Stack;
+
+void build_array(int** graph, Vert vert[], int max);
+void push(int v, Stack s, int max);
+int pop(Stack s);
+void dfs(int v, int transpose, Vert vert[]);
+void reset_visited(int max, Vert vert[]);
+void order_pass(int max, Vert vert[]);
+void scc_pass(Stack s, Vert vert[]);
 
 int main (int argc, char* argv[]) {
 
@@ -45,19 +62,12 @@ int main (int argc, char* argv[]) {
 		++index;
 	}	
 
-    int magnitude[max];
-    int check[max];
-    for (int i = 0; i < max; i++) {
-        magnitude[i] = 0;
-        check[i] = 0;
-    }
-    build_array(input, magnitude, check, max);
+    Vert vertices[max];
+    Stack stack = {-1, {}};
 
-    for(int i = 0; i < max; i++){
-        if (check[i] == 1){
-            printf("%d ", i);
-        }
-    }
+    build_array(input, vertices, max);
+
+    
 
     fclose(inptr);
     free(input);
@@ -66,16 +76,60 @@ int main (int argc, char* argv[]) {
     return 0;
 }
 
-void build_array (int** graph, int magnitude[], int check[], int max) {
-    int index = 0;
-    int temp;
-    while(*(graph + index) != NULL){
-        if(index == 0 || index % 2 == 0){
-            temp =(**(graph + index));
-            ++magnitude[temp - 1];
-            check[magnitude[temp -1]] = 1;
+void build_array (int** graph, Vert vert[], int max) {
+    
+}
+
+void push(int v, Stack s, int max){
+    s.top++;
+    if(s.top < max){
+        s.elements[s.top] = v;
+    } else{
+        printf("Stack is full\n");
+        exit(1);
+    }
+}
+
+int pop(Stack s){
+    return s.top < 0 ? -1 : s.elements[s.top--];
+}
+
+void dfs(int v, int transpose, Vert vert[]){
+    int i, c ,n;
+    vert[v].visited = 1;
+    for(i = 0, c = vert[v].magnitude; i < c; ++i){
+        n = vert[v].adj[i]*transpose;
+
+        if(n > 0){
+            if(!vert[n-1].visited){
+                dfs(n-1, transpose, vert);
+            }
         }
-        ++index;
+    }
+}
+
+void reset_visited(int max, Vert vert[]){
+    for(int i = 0; i < max; ++i){
+        vert[i].visited = 0;
+    }
+}
+
+void order_pass(int max, Vert vert[]){
+    for(int i = 0; i < max; ++i){
+        if(!vert[i].visited){
+            dfs(i , -1, vert);
+        }
+    }
+}
+
+void scc_pass(Stack s, Vert vert[]){
+    int i = 0, v;
+    while((v = pop(s)) != -1){
+        if(!vert[v].visited){
+            printf("scc %d", ++i);
+            dfs(v, 1, vert);
+            printf("\n");
+        }
     }
 }
 
