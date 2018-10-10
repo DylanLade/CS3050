098f6ac6a83ca537f35c57f45f69d980125a0937
William Givens <williamgivens@Williams-MacBook-Air.local>
2018-10-10 04:09:12 -0500

    added 3050HW2FINAL.c

--- 3050HW2.c
+++ 3050HW2.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define MAX_VERT  20
 
@@ -50,6 +51,7 @@ int main (int argc, char* argv[]) {
     int index = 0;
 
     //Read in input file into input
+    printf("File input: \n");
     FILE *inptr = fopen(inputFile, "r");
     while (0 == 0) {
 		if (feof(inptr)) {
@@ -62,53 +64,87 @@ int main (int argc, char* argv[]) {
         if (max < **(input + index)) {
             max = **(input + index);
         }
+      printf("%d ", **(input + index));
 		++index;
 	}
-    // printf("Max: %d", max);
+    printf("Max: %d", max);
     printf("\n");
+    printf("%d\n", index);
 
     Vert vertices[max];
     Stack* stack = malloc(sizeof(stack));
+    printf("Create Stack pointer\n");
     stack->top = -1;
+    printf("Initialize top\n");
    
     for(int i = 0; i <= max + 1; i++){
-        stack->elements[i]  = ' ';
+      stack->elements[i]  = ' ';
     }
+    printf("Stack init works\n");
 
-    for(int i = 0; i < max; i++){
-        vertices[i].visited = 0;
-        vertices[i].magnitude = 0;
-        for(int j = 0; j < MAX_VERT; j++){
-            vertices[i].adj[j] = ' ';
-        } 
-    }
+   for(int i = 0; i < max; i++){
+      vertices[i].visited = 0;
+      vertices[i].magnitude = 0;
+      for(int j = 0; j < MAX_VERT; j++){
+          vertices[i].adj[j] = ' ';
+      }
+    //   printf("Visited: %d Magnitude: %d Index: %d\n", vertices[i].visited, vertices[i].magnitude, i);
+   }
 
+   printf("Vert init works\n");
     index = 0;
-    while((*(input+index)) != NULL){
-        if(index == 0 || index % 2 == 0){
-            int source = **(input+index);
-            int dest = **(input+index+1);
-            build_array(source, dest, vertices, max);
+
+   while((*(input+index)) != NULL){
+      if(index == 0 || index % 2 == 0){
+         int source = **(input+index);
+         int dest = **(input+index+1);
+         build_array(source, dest, vertices, max);
+         printf("s: %d | d: %d\n", source, dest);
+      }
+      index++;
+   }
+
+//    printf("After build_array Loop\n");
+//     int temp = 43434213;
+//     for(index = 0; index < max; index++) {
+//         for(int i = 0; i < vertices[index].magnitude; i++) {
+//             if(abs(vertices[index].adj[i]) > abs(vertices[index].adj[i+1])) {
+//                 temp = vertices[index].adj[i];
+//                 vertices[index].adj[i] = vertices[index].adj[i+1];
+//                 vertices[index].adj[i+1] = vertices[index].adj[i];
+//             }
+//         }
+        
+//     }
+    int i = 0;
+    for(index = 0; index < max; index++) {
+        printf("v: %d | ", index + 1);
+        i = 0;
+        while(vertices[index].adj[i] != ' ') {
+            printf("%d ", vertices[index].adj[i]);
+            i++;
         }
-        index++;
+        printf("\n");
     }
 
     order_pass(max, vertices, stack);
     reset_visited(max, vertices);
     scc_pass(stack, vertices, max);
 
-    fclose(inptr);
-    free(input);
+   fclose(inptr);
+   free(input);
 
-    // printf("\n");
-    return 0;
+   printf("\n");
+   return 0;
 }
 
 void build_array (int s, int d, Vert* vert, int max) {
-    int i = 0;
-    int temp;
-    vert[s-1].magnitude++;
-    vert[d-1].magnitude++;
+   int i = 0;
+   int temp;
+   vert[s-1].magnitude++;
+   vert[d-1].magnitude++;
+   //printf("mag: %d | ", vert[s-1].magnitude);
+   //printf(". ");
     while(vert[s-1].adj[i] != ' '){
       i++;
     }
@@ -120,10 +156,12 @@ void build_array (int s, int d, Vert* vert, int max) {
     }
     vert[d-1].adj[j] = -s;
 
+    printf("pos: %d | neg: %d \n", vert[s-1].adj[i], vert[d-1].adj[j]);
 }
 
 void push(int v, Stack* stack, int max){
     stack->top++;
+    // printf("Stack Top: %d ", stack->top);
     if(stack->top < max){
         stack->elements[stack->top] = v;
     } else{
@@ -146,6 +184,9 @@ void dfs(int v, int transpose, Vert vert[], Stack* stack, int max){
             if(!vert[n-1].visited){
                 dfs(n-1, transpose, vert, stack, max);
             }
+            // else{
+            //     printf("v: %d\n", vert);
+            // }
         }
     }
     if (transpose < 0){
@@ -157,22 +198,27 @@ void dfs(int v, int transpose, Vert vert[], Stack* stack, int max){
 
 void reset_visited(int max, Vert vert[]){
     for(int i = 0; i < max; ++i){
+        // printf("Reset visited to 0 ");
         vert[i].visited = 0;
     }
+    printf("\n");
 }
 
 void order_pass(int max, Vert vert[], Stack* stack){
     for(int i = 0; i < max; ++i){
         if(!vert[i].visited){
+            // printf("Ordered Pass DFS ");
             dfs(i , -1, vert, stack, max);
         }
     }
+
+    printf("\n");
 }
 
 void scc_pass(Stack* stack, Vert vert[], int max){
     int i = 0, v;
     while((v = pop(stack)) != -1){
-        // printf("SCC pass ");
+        //printf("v: %d\n", v);
         if(!vert[v].visited){
             printf("scc %d: ", ++i);
             dfs(v, 1, vert, stack, max);
